56b2291584641d8852b25d0bbc9f0226cb3e2272
C.H <oh6for@gmail.com>
2021-09-19 01:15:00 +0300

    -Add early test script, for steering.

--- Toyota-8FG.gd
+++ Toyota-8FG.gd
@@ -10,4 +10,13 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_LEFT:
+			get_node("Rear_Wheel_Right_Joint").set("angular_motor_y/target_velocity", 2)
+			get_node("Rear_Wheel_Left_Joint").set("angular_motor_y/target_velocity", 2)
+		elif event.pressed and event.scancode == KEY_RIGHT:
+			get_node("Rear_Wheel_Right_Joint").set("angular_motor_y/target_velocity", -2)
+			get_node("Rear_Wheel_Left_Joint").set("angular_motor_y/target_velocity", -2)
+		#else: get_node("Rear_Wheel_Right_Joint").set("angular_motor_y/target_velocity", 0)
+		#get_node("Rear_Wheel_Left_Joint").set("angular_motor_y/target_velocity", 0)  
